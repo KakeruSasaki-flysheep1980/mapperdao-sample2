4a013a17327881c80514e1140e3d50cdf51a49e0
sasaki <sasaki@furyu.jp>
2012-11-01 15:54:57 +0900

    コメント追記

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,11 +8,17 @@ object Application extends Controller {
 		import play.api.Play.current
 		import com.googlecode.mapperdao.utils.Setup
 
+    // データベース設定を読み込む
 		val (jdbc, mapperdao, queryDao, txManager) = Setup.h2(play.api.db.DB.getDataSource(), List(models.UserEntity))
 
+    // mapperdaoはDDL発行はしてくれない??
+    // evolutionで作成する -> play run -> ページアクセスで作成可
+
+    // テキトーにデータ登録
 		val now = new java.util.Date
 		mapperdao.insert(models.UserEntity, new models.User(1L, "__name__", now, now))
 
+    // テキトーに検索
 		val selected = mapperdao.select(models.UserEntity, 1L)
 		println("%s".format(selected))
 
--- app/models/user.scala
+++ app/models/user.scala
@@ -3,7 +3,11 @@ package models
 import java.util.Date
 import com.googlecode.mapperdao._
 
+// ドメインモデル
 case class User(id: Long, name: String, createdTime: Date, updatedTime: Date)
+
+// ドメインモデルとデータベースのマッピングを責務とするクラス
+// テーブル名, 列名はここで定義可能
 object UserEntity extends Entity[Long, NaturalLongId, User]("User") {
 	val id = key("id") to (_.id)
 	val name = column("name") to (_.name)
