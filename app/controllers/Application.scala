0574e4bb7740e4ec401bf070aff5764fa9af94ce
sasaki <sasaki@furyu.jp>
2012-11-06 09:36:13 +0900

    Daoを使った実装をしてみる

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,19 +1,21 @@
 package controllers
 
 import play.api.mvc._
+import com.googlecode.mapperdao.{ NaturalLongId, QueryDao, MapperDao }
+import com.googlecode.mapperdao.utils.{ TransactionalCRUD, All, TransactionalSurrogateLongIdCRUD }
 
 object Application extends Controller {
 
+	import play.api.Play.current
+	import com.googlecode.mapperdao.utils.Setup
+
+	// データベース設定を読み込む
+	val (jdbc, mapperdao, queryDao, txManager) = Setup.h2(play.api.db.DB.getDataSource(), List(models.UserEntity))
+
+	// mapperdaoはDDL発行はしてくれない??
+	// evolutionで作成する -> play run -> ページアクセスで作成可
+
 	def index = Action {
-		import play.api.Play.current
-		import com.googlecode.mapperdao.utils.Setup
-
-		// データベース設定を読み込む
-		val (jdbc, mapperdao, queryDao, txManager) = Setup.h2(play.api.db.DB.getDataSource(), List(models.UserEntity))
-
-		// mapperdaoはDDL発行はしてくれない??
-		// evolutionで作成する -> play run -> ページアクセスで作成可
-
 		// テキトーにデータ登録
 		val now = new java.util.Date
 		mapperdao.insert(models.UserEntity, new models.User(1L, "__name__", now, now))
@@ -30,7 +32,33 @@ object Application extends Controller {
 		mapperdao.delete(models.UserEntity, updated)
 		println("deleted: %s".format(mapperdao.select(models.UserEntity, 1L)))
 
-		Ok(jp.furyu.play.velocity.mvc.VM("vm/index.vm"))
+		Ok
+	}
+
+	def index2 = Action {
+		val now = new java.util.Date
+		val userDao = new UserDao
+
+		val created = userDao.create(new models.User(2L, "__name__", now, now))
+		println("created: %s".format(created))
+
+		val retrieved = userDao.retrieve(2L)
+		println("retrieved: %s".format(retrieved))
+
+		val updated = userDao.update(retrieved.get, new models.User(2L, "__updated__", now, now))
+		println("updated: %s".format(updated))
+
+		userDao.delete(2L)
+		println("retrieved: %s".format(userDao.retrieve(2L)))
+
+		Ok
+	}
+
+	class UserDao extends TransactionalCRUD[Long, NaturalLongId, models.User] with All[Long, NaturalLongId, models.User] {
+		val entity = models.UserEntity
+		val queryDao = Application.queryDao
+		val mapperDao = Application.mapperdao
+		val txManager = Application.txManager
 	}
 
 }
