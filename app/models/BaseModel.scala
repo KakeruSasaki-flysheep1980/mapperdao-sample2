5e0f89a971f5835e872c1bcb4f2fd12344556d0b
sasaki <sasaki@furyu.jp>
2012-11-15 12:29:44 +0900

    BaseModelを分割

--- app/models/BaseModel.scala
+++ app/models/BaseModel.scala
@@ -2,8 +2,13 @@ package models
 
 import java.util.Date
 
-trait BaseModel {
+trait BaseModel extends Sequenced with RecorededTime
+
+trait Sequenced {
 	val id: Long
+}
+
+trait RecorededTime {
 	val createdTime: Date
 	val updatedTime: Date
 }
