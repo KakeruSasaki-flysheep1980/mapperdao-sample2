f4b415763101807d8fefebf9cad946da5be29989
sasaki <sasaki@furyu.jp>
2012-11-15 12:22:38 +0900

    implement UserDao

--- app/models/user.scala
+++ app/models/user.scala
@@ -4,14 +4,4 @@ import java.util.Date
 import com.googlecode.mapperdao._
 
 // ドメインモデル
-case class User(id: Long, name: String, createdTime: Date, updatedTime: Date)
-
-// ドメインモデルとデータベースのマッピングを責務とするクラス
-// テーブル名, 列名はここで定義可能
-object UserEntity extends Entity[Long, NaturalLongId, User]("User") {
-	val id = key("id") to (_.id)
-	val name = column("name") to (_.name)
-	val createdTime = column("created_time") to (_.createdTime)
-	val updatedTime = column("updated_time") to (_.updatedTime)
-	def constructor(implicit m: com.googlecode.mapperdao.ValuesMap) = new User(id, name, createdTime, updatedTime) with NaturalLongId
-}
+case class User(id: Long, name: String, createdTime: Date, updatedTime: Date) extends BaseModel
