5c42fbeacc3c31e2fe6166598cb81430d9b89ec2
sasaki <sasaki@furyu.jp>
2012-11-15 14:54:50 +0900

    rename User.scala

--- app/models/user.scala
+++ app/models/user.scala
@@ -1,7 +1,6 @@
 package models
 
 import java.util.Date
-import com.googlecode.mapperdao._
 
 // ドメインモデル
 case class User(id: Long, name: String, createdTime: Date, updatedTime: Date) extends BaseModel
